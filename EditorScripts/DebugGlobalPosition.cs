7113e00590df8d1780a02dad886a1879c5afa712
Jan0211 <jan.pixberg123@gmail.com>
2022-05-22 22:03:13 +0200

    added base singleton, added editor tool to debug global positions

---
+++ EditorScripts/DebugGlobalPosition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityTools.EditorScripts
+{
+    public static class DebugGlobalPosition
+    {
+        [MenuItem("Debug/GlobalPosition")]
+        public static void DisplayGlobalPosition()
+        {
+            if (Selection.activeGameObject == null) return;
+            var pos = Selection.activeGameObject.transform.position;
+            Debug.Log($"GlobalPosition: {pos}");
+        }
+    }
+}
---
+++ General/UnitySingleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UnityTools.General
+{
+    /// <summary>
+    /// Inherit from this class to create a simple singleton
+    /// </summary>
+    /// <typeparam name="T">the singleton class</typeparam>
+    public class UnitySingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        // ReSharper disable once MemberCanBePrivate.Global
+        public static T Instance { get; private set; }
+
+        protected  virtual void Awake()
+        {
+            if (Instance != null && Instance != this) Destroy(this);
+            else Instance = this as T;
+        }
+    }
+}
