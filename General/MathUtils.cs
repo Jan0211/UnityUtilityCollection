de3f0f6c85ea95e47742c3d21c0f881be83f4d48
Jan0211 <jan.pixberg123@gmail.com>
2022-05-25 10:07:41 +0200

    added MathUtils class, added Remap method

---
+++ General/MathUtils.cs
@@ -0,0 +1,18 @@
+﻿namespace UnityTools.General
+{
+    public static class MathUtils
+    {
+        /// <summary>
+        ///     Maps a value from an origin range to a target range.
+        /// </summary>
+        /// <param name="value">The value that will be mapped</param>
+        /// <param name="originMin">The original range's lower bound.</param>
+        /// <param name="originMax">The original range's upper bound.</param>
+        /// <param name="targetMin">The target range's lower bound.</param>
+        /// <param name="targetMax">The target range's upper bound.</param>
+        private static float Remap(float value, float originMin, float originMax, float targetMin, float targetMax)
+        {
+            return (value - originMin) / (originMax - originMin) * (targetMax - targetMin) + targetMin;
+        }
+    }
+}
